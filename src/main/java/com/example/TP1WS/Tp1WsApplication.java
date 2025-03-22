edaee6c4fdfcdf58002819322754720b1c06a4c7
Arvindesss <77747376+Arvindesss@users.noreply.github.com>
2025-03-22 22:46:32 +0100

    add swagger

--- src/main/java/com/example/TP1WS/Tp1WsApplication.java
+++ src/main/java/com/example/TP1WS/Tp1WsApplication.java
@@ -1,9 +1,18 @@
 package com.example.TP1WS;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "TP1 WS",
+				description = "TP1 endpoints and apis",
+				version = "1.0.0"
+		)
+)
 public class Tp1WsApplication {
 
 	public static void main(String[] args) {
